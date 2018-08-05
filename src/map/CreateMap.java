95e43126ff35b877b395a7901073b000aac90436
ekuss <hoge@hoge.com>
2018-08-05 21:56:28 +0900

    createmapメソッドを実装

--- src/map/CreateMap.java
+++ src/map/CreateMap.java
@@ -34,12 +34,31 @@ public class CreateMap {
 	 */
 	public static Contains[][] createmap(ArrayList<String> data){
 		Contains[][] map = new Contains[10][10];
+		ArrayList<String> temp = new ArrayList<>();
 		for(String line: data) {
-			String[] lineData = line.split(" ");
-			for(int i = 0; i < lineData.length; i++) {
-				System.out.print(lineData[i]);
-				if(i == 9) System.out.println();
-				
+			for(String cha: line.split(" ")){
+				temp.add(cha);
+			}
+		}
+		
+		//ArrayListのtempにちゃんと記号入ってるかチェック
+		for(int i = 0; i < temp.size(); i++) {
+			System.out.print(temp.get(i));
+			if(i%10 == 9) System.out.println();
+		}
+		
+		//map[][]にアルファベットに対応したオブジェクトを生成, 代入
+		int k=0;
+		for(int i=0; i<9; i++){
+			for(int j=0; j<9; j++){
+				if(temp.get(k).equals("H")) map[i][j] = new Home(0);
+				else if(temp.get(k).equals("P")) map[i][j] = new Path(0);
+				else if(temp.get(k).equals("S")) map[i][j] = new Store(0);
+				else if(temp.get(k).equals("E")) map[i][j] = new Station(0);
+				else {
+					System.out.println("mapに無効な値が指定されました");
+					System.exit(0);
+				}
 			}
 		}
 		

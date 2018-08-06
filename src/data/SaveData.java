8161e3f7170f0d7d1856057fe51aa86b1d1db8cc
niya1123 <yoheikurioka@gmail.com>
2018-08-06 22:52:00 +0900

    @@@

--- src/data/SaveData.java
+++ src/data/SaveData.java
@@ -1,9 +1,20 @@
 package data;
 
+import java.util.ArrayList;
+
 /**
  * 各種データを保持するクラス.
  * @author YK
  */
 public class SaveData {
+	
+	/**
+	 * ReadMapのゲッター
+	 * @param name　map?.txtの？の部分
+	 * @return　ファイルから読み込んだArrayList
+	 */
+	public ArrayList<String> getReadMap(String name){
+		return ReadMap.readMap(name);
+	}
 	
 }

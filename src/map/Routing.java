b36037caea25f036f0345e1fa6337b0b071e7625
ekuss <anija7771@yahoo.co.jp>
2018-08-09 02:43:22 +0900

    makeRouteメソッドの実装

--- src/map/Routing.java
+++ src/map/Routing.java
@@ -1,5 +1,6 @@
 package map;
 
+
 /**
  * 道筋のGUI描画の際に利用
  * @author kei
@@ -28,6 +29,7 @@ public class Routing {
 	}
 
 
+	
 	public void through(int from, int to) {
 		if(from==1 || to== 1) {
 			north= true;
@@ -71,7 +73,121 @@ public class Routing {
 	 * @return
 	 */
 	public static Contains[][] makeRoute(Contains[][] contains){
-
+		
+		//orderの最大値を取ってくる
+		int maxOrder=0;
+		for(int i = 0; i < contains.length; i++) {
+			for(int j = 0; j < contains[i].length; j++) {
+				if(contains[i][j].getOrder() < maxOrder){
+					maxOrder = contains[i][j].getOrder();
+				}
+			}
+		}
+		
+		//orderのカウンター
+		int countOrder=2;
+		
+		//１つ目のノードの添字
+		int y1=0;
+		int x1=0;
+		
+		//２つ目のノードの添字
+		int y2=0;
+		int x2=0;
+		
+		// 1→2, 2→3ってずらして処理する
+		while(maxOrder>0){
+			
+			//orderを探す
+			for(int i = 0; i < contains.length; i++) {
+				for(int j = 0; j < contains[i].length; j++) {
+					if(contains[i][j].getOrder() == countOrder){
+						y2 = i;
+						x2 = j;
+					}
+				}
+			}
+			
+			//→↓
+			if ( (x2-x1)>=0 && (y2-y1)>=0 ) {
+				//→と↓に移動する回数
+				int rangeX=Math.abs(x2-x1);//→にrangeX回 3
+				int rangeY=Math.abs(y2-y1)-1;//↓にrangeY回 3
+				
+				//→
+				for(int i=1; i<=rangeX; i++) {
+					contains[y1][x1+i].getRouting().through(3, 2);
+					if(i==rangeX) contains[y1][x1+i].getRouting().through(3, 4);
+				}
+				
+				//↓
+				for(int j=1; j<=rangeY; j++) {
+					contains[y1+j][x1+rangeX].getRouting().through(1, 4);
+				}
+			}
+			
+			//→↑
+			if( (x2-x1)>=0 && (y2-y1)<0 ) {
+				//→と↑に移動する回数
+				int rangeX=Math.abs(x2-x1);//→にrangeX回
+				int rangeY=Math.abs(y2-y1)-1;//↓にrangeY回
+				
+				//→
+				for(int i=1; i<=rangeX; i++) {
+					contains[y1][x1+i].getRouting().through(3, 2);
+					if(i==rangeX) contains[y1][x1+i].getRouting().through(3, 1);
+				}
+				
+				//↑
+				for(int j=1; j<=rangeY; j++) {
+					contains[y1-j][x1+rangeX].getRouting().through(4, 1);
+				}
+			}
+			
+			//←↓
+			if( (x2-x1)<0 && (y2-y1)>=0 ){
+				//→と↑に移動する回数
+				int rangeX=Math.abs(x2-x1);//→にrangeX回
+				int rangeY=Math.abs(y2-y1)-1;//↓にrangeY回
+				
+				//←
+				for(int i=1; i<=rangeX; i++) {
+					contains[y1][x1-i].getRouting().through(2, 3);
+					if(i==rangeX) contains[y1][x1-i].getRouting().through(2, 4);
+				}
+				
+				//↓
+				for(int j=1; j<=rangeY; j++) {
+					contains[y1+j][x1+rangeX].getRouting().through(4, 1);
+				}
+			}
+			
+			//←↑
+			if( (x2-x1)<0 && (y2-y1)<0 ){
+				//→と↑に移動する回数
+				int rangeX=Math.abs(x2-x1);//→にrangeX回
+				int rangeY=Math.abs(y2-y1)-1;//↓にrangeY回
+				
+				//←
+				for(int i=1; i<=rangeX; i++) {
+					contains[y1][x1-i].getRouting().through(2, 3);
+					if(i==rangeX) contains[y1][x1-i].getRouting().through(2, 4);
+				}
+				
+				//↑
+				for(int j=1; j<=rangeY; j++) {
+					contains[y1-j][x1+rangeX].getRouting().through(4, 1);
+				}
+			}
+			
+			//次のために添え字の設定
+			y1 = y2;
+			x1 = x2;
+			
+			//カウント
+			countOrder++;
+			maxOrder--;
+		}
 
 		return contains;
 	}
